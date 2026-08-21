baaa0037c70542649948ccd01f3f8f07020440e0
aishwaryap27 <aishwaryapramod23cs@rnsit.ac.in>
2026-08-21 23:33:32 +0530

    mouth aspect ratio and yawn detection added along with ear and blink counter

--- face_detection.py
+++ face_detection.py
@@ -1,7 +1,60 @@
 import cv2
 import mediapipe as mp
+import math
+import time
 
+def calculate_ear(landmarks, eye_indices):
 
+    # Get the 6 eye points
+    p1 = landmarks[eye_indices[0]]
+    p2 = landmarks[eye_indices[1]]
+    p3 = landmarks[eye_indices[2]]
+    p4 = landmarks[eye_indices[3]]
+    p5 = landmarks[eye_indices[4]]
+    p6 = landmarks[eye_indices[5]]
+
+    # Vertical distances
+    vertical_1 = math.dist(
+        (p2.x, p2.y),
+        (p6.x, p6.y)
+    )
+
+    vertical_2 = math.dist(
+        (p3.x, p3.y),
+        (p5.x, p5.y)
+    )
+
+    # Horizontal distance
+    horizontal = math.dist(
+        (p1.x, p1.y),
+        (p4.x, p4.y)
+    )
+
+    # Eye Aspect Ratio
+    ear = (vertical_1 + vertical_2) / (2 * horizontal)
+
+    return ear
+def calculate_mar(landmarks):
+
+    left_corner = landmarks[61]
+    right_corner = landmarks[291]
+
+    upper_lip = landmarks[13]
+    lower_lip = landmarks[14]
+
+    vertical = math.dist(
+        (upper_lip.x, upper_lip.y),
+        (lower_lip.x, lower_lip.y)
+    )
+
+    horizontal = math.dist(
+        (left_corner.x, left_corner.y),
+        (right_corner.x, right_corner.y)
+    )
+
+    mar = vertical / horizontal
+
+    return mar
 FACE_DETECTOR_MODEL = "models/blaze_face_short_range.tflite"
 FACE_LANDMARKER_MODEL = "models/face_landmarker.task"
 
@@ -34,14 +87,11 @@ face_landmarker = mp.tasks.vision.FaceLandmarker.create_from_options(
     landmarker_options
 )
 
-
-# ==============================
-# CAMERA
-# ==============================
-
 cap = cv2.VideoCapture(0)
-
-
+yawn_start_time = None
+yawn_count = 0
+blink_count = 0
+eyes_were_closed = False
 while True:
 
     success, frame = cap.read()
@@ -61,13 +111,15 @@ while True:
     )
 
 
-    # ==============================
-    # FACE DETECTION
-    # ==============================
-
+    
     detection_result = face_detector.detect(mp_image)
 
     for detection in detection_result.detections:
+
+        score = detection.categories[0].score
+
+        if score < 0.75:
+            continue
 
         bbox = detection.bounding_box
 
@@ -85,33 +137,124 @@ while True:
         )
 
 
-    # ==============================
-    # FACE LANDMARKS
-    # ==============================
 
     landmark_result = face_landmarker.detect(mp_image)
 
 
-    # ==============================
-    # EYE LANDMARKS
-    # ==============================
+    
 
     if landmark_result.face_landmarks:
 
         for face_landmarks in landmark_result.face_landmarks:
 
-            # Left eye
+            mar = calculate_mar(face_landmarks)
+            cv2.putText(
+            frame,
+            f"MAR: {mar:.2f}",
+            (30, 200),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1,
+            (255, 0, 255),
+            2
+            )
             left_eye_indices = [
                 33, 160, 158, 133, 153, 144
             ]
 
-            # Right eye
+            
             right_eye_indices = [
                 362, 385, 387, 263, 373, 380
             ]
+            left_ear = calculate_ear(
+            face_landmarks,
+            left_eye_indices
+            )
 
+            right_ear = calculate_ear(
+            face_landmarks,
+            right_eye_indices
+            )
 
-            # Draw left eye landmarks
+            average_ear = (left_ear + right_ear) / 2
+           # average_ear = (left_ear + right_ear) / 2
+
+            EAR_THRESHOLD = 0.20
+
+            if average_ear < EAR_THRESHOLD:
+                eye_status = "CLOSED"
+            else:
+                eye_status = "OPEN"
+            if eye_status == "CLOSED":
+                eyes_were_closed = True
+
+            elif eye_status == "OPEN" and eyes_were_closed:
+                blink_count += 1
+                eyes_were_closed = False
+            MAR_THRESHOLD = 0.40
+
+            if mar > MAR_THRESHOLD:
+                mouth_status = "OPEN"
+            else:
+                mouth_status = "CLOSED"
+            if mouth_status == "OPEN":
+
+                if yawn_start_time is None:
+                    yawn_start_time = time.time()
+
+                mouth_open_duration = time.time() - yawn_start_time
+
+            else:
+                yawn_start_time = None
+                mouth_open_duration = 0
+            if mouth_open_duration > 1.5:
+                yawn_status = "YAWN DETECTED"
+            else:
+                yawn_status = "NO YAWN"
+            cv2.putText(
+            frame,
+            f"Eye Aspect Ratio: {average_ear:.2f}",
+            (30, 40),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1,
+            (0, 255, 255),
+            2
+            )
+            cv2.putText(
+            frame,
+            f"Eyes: {eye_status}",
+            (30, 80),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1,
+            (0, 255, 0),
+            2
+            )
+            cv2.putText(
+            frame,
+            f"Blinks: {blink_count}",
+            (30, 120),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1,
+            (255, 255, 0),
+            2
+            )
+            cv2.putText(
+            frame,
+            yawn_status,
+            (30, 280),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1,
+            (0, 0, 255),
+            2
+            )
+            cv2.putText(
+            frame,
+            f"Mouth: {mouth_status}",
+            (30, 240),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1,
+            (255, 0, 255),
+            2
+            )
             for index in left_eye_indices:
 
                 landmark = face_landmarks[index]
@@ -133,7 +276,6 @@ while True:
                 )
 
 
-            # Draw right eye landmarks
             for index in right_eye_indices:
 
                 landmark = face_landmarks[index]
